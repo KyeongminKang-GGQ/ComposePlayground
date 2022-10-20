a0097e549aec354bc8efb95514d12a9636eaca68
TODO <todo-actions[bot]@users.noreply.github.com>
2022-10-20 09:04:40 +0000

    Update TODO references: #68

--- app/src/main/java/com/example/composeplayground/ui/theme/Hello.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/Hello.kt
@@ -3,5 +3,5 @@ package com.example.composeplayground.ui.theme
 class Hello {
     // TODO [#53]: Title test comments test
     // TODO [#66]: Please add milestone..
-    // TODO [$63510f227fdc48000875fbe9]: Add milestone graphQL test
+    // TODO [#68]: Add milestone graphQL test
 }
