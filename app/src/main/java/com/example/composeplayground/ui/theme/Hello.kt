f0a058d6193a3e529a69a54626bb4efab44b5f80
TODO <todo-actions[bot]@users.noreply.github.com>
2022-10-20 08:41:22 +0000

    Update TODO references: #66, #67

--- app/src/main/java/com/example/composeplayground/ui/theme/Hello.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/Hello.kt
@@ -2,5 +2,5 @@ package com.example.composeplayground.ui.theme
 
 class Hello {
     // TODO [#53]: Title test comments test
-    // TODO [$635109adaf3e3c0008b2ea6c]: Please add milestone..
+    // TODO [#66]: Please add milestone..
 }
--- app/src/main/java/com/example/composeplayground/ui/theme/New.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/New.kt
@@ -1,7 +1,7 @@
 package com.example.composeplayground.ui.theme
 
 class New {
-    // TODO [$635109adaf3e3c0008b2ea6d]: 아래 부분 수정 필요함
+    // TODO [#67]: 아래 부분 수정 필요함
     //
     // PROD 환경에서 url 변경 필요
     //
