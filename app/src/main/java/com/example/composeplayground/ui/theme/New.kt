e727c23e16f747f6fe3dc159a49197c2a47c2f9c
TODO <todo-actions[bot]@ggq.gg>
2022-10-20 12:51:03 +0000

    Update TODO references: #73

--- app/src/main/java/com/example/composeplayground/ui/theme/New.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/New.kt
@@ -1,7 +1,7 @@
 package com.example.composeplayground.ui.theme
 
 class New {
-    // TODO [$63514432df1fb500086883d0]: Need implements New.kt
+    // TODO [#73]: Need implements New.kt
     //
     // Please implement here
 }
