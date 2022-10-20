07968d1c23f790b6167dabdfc85bc49daacdee31
TODO <todo-actions[bot]@users.noreply.github.com>
2022-10-20 09:04:34 +0000

    Collect TODO comments

--- app/src/main/java/com/example/composeplayground/ui/theme/Hello.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/Hello.kt
@@ -3,5 +3,5 @@ package com.example.composeplayground.ui.theme
 class Hello {
     // TODO [#53]: Title test comments test
     // TODO [#66]: Please add milestone..
-    // TODO: Add milestone graphQL test
+    // TODO [$63510f227fdc48000875fbe9]: Add milestone graphQL test
 }
