6f1e43af2368d939c8175203257f097d6262774a
KyeongminKang-GGQ <kyeongmin.kang@ggq.gg>
2022-10-24 14:49:51 +0900

    ManualCreateIssuesFromTodo 추가

---
+++ app/src/main/java/com/example/composeplayground/ui/theme/Welcome.kt
@@ -0,0 +1,12 @@
+package com.example.composeplayground.ui.theme
+
+class Welcome {
+    // TODO: [TODO] Need to implements Hello.kt
+    //
+    // I need to implements Hello.kt
+    // These includes:
+    // - `Welcome.kt`
+    // - and Unit Test for `WelcomeWorker.kt`
+    //
+    // Please implements it until v2.1.1
+}
