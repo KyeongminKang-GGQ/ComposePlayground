8527091a1989260e4ca625152bd2ea30a6b79e70
KyeongminKang-GGQ <kyeongmin.kang@ggq.gg>
2022-10-27 10:47:41 +0900

    comment test

--- app/src/main/java/com/example/composeplayground/ui/theme/New.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/New.kt
@@ -1,4 +1,11 @@
 package com.example.composeplayground.ui.theme
 
 class New {
+    // TODO: comment test
+    // I have to implement this
+    // ```.javascript
+    // const absolutePath = path.join(process.cwd(), filename);
+    // const newComments = parsePatch(enhancedTags, patch);
+    // let parsedComments: Comments = {};
+    // ```
 }
